0417c01b7ed009df30ae8cdcb27dad80adefcc28
秋庭圭吾 <akibakeigo@229.240.231.10.ty1.coins.jp.ricoh.com>
2022-07-22 10:36:09 +0900

    conf_first commit

---
+++ test2/test2/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  test2
+//
+//  Created by 秋庭圭吾 on 2022/07/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func changeColor(_ sender: Any) {
+        view.backgroundColor = UIColor.red
+    }
+    
+    @IBAction func changeColor2(_ sender: Any) {
+        view.backgroundColor = UIColor.black
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
